ccc04722e3d8c6566587174d11806ed69f1b0479
Pavel Gun <pavel_gun@epam.com>
2021-04-30 13:15:50 +0300

    fix / tests

--- src/test/java/com/example/demoaws/DemoAwsApplicationTests.java
+++ src/test/java/com/example/demoaws/DemoAwsApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.demoaws;
-/*
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoAwsApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    public void testRequest() throws Exception {
+        MvcResult result = this.mockMvc.perform(post("/handle")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("test"))
+                .andReturn();
+        Assertions.assertTrue(mockMvc.perform(asyncDispatch(result))
+                .andReturn()
+                .getResponse()
+                .getContentAsString()
+                .matches("[-+]?\\d+"));
     }
 
-}*/
+}
